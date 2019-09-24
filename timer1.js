0132bd900d55ba1641aacf8e844a201f2576ed8c
Ha Pham <hapham0392@gmail.com>
2019-09-24 20:01:25 +0000

    simple timer without the beep, but with string @peterhanng @jimhjkim

--- timer1.js
+++ timer1.js
@@ -1,9 +1,43 @@
 //console.log(process.argv.slice(2))
 let input = process.argv.slice(2).filter((element) => element > 0)
+
+input.forEach((num, i, arr) => {
+  setTimeout(() => {
+    process.stdout.write('.')
+    if (arr.length - 1 - i == 0) {
+      console.log("")
+    }
+  }, num * 1000);
+});
+
 //console.log(input);
-let timer = function(arg) {
-  setTimeout(() => {process.stdout.write(".  ")}, arg*1000)
-  }
-  input.forEach(function(wait) {
-  timer(wait);
-})
+// let timer = function(seconds, callback) {
+//   setTimeout(function() {
+//     process.stdout.write(".   ")
+//     callback();
+//   }, seconds*1000)
+//     // console.log(numLeft)
+//     // if (numLeft == 0) {
+//       // process.stdout.write('\n')
+// };
+
+// input.forEach(function(num) {
+//   timer(num, function() { console.log() } )
+// });
+
+// document.write('\n');
+
+// let timer = function(seconds, numLeft) {
+//   setTimeout(function() {
+//     process.stdout.write(".   ")}, seconds*1000)
+//     process.stdout.write('\n')
+//     console.log(numLeft)
+//     if (numLeft == 0) {
+//       // process.stdout.write('\n')
+//     }
+// };
+// input.forEach(function(num, i, arr) {
+//   timer(num, arr.length - 1 - i);
+// });
+
+// document.write('\n');
